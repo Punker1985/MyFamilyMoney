b034a5930ca45c997c64bedf51481df8ddfc5e8e
Punker1985 <92338741+Punker1985@users.noreply.github.com>
2022-02-05 12:50:38 +0800

    add Transfer model

--- src/main/java/com/example/MyFamilyMoney/models/Transfer.java
+++ src/main/java/com/example/MyFamilyMoney/models/Transfer.java
@@ -1,4 +1,69 @@
 package com.example.MyFamilyMoney.models;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
 public class Transfer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private Account accountSpending;
+    @ManyToOne
+    private Account accountReceipt;
+    private Long amount;
+    private LocalDateTime date;
+    private String description;
+
+    public Transfer() {
+    }
+
+    public Transfer(Account accountSpending, Account accountReceipt, Long amount, LocalDateTime date, String description) {
+        this.accountSpending = accountSpending;
+        this.accountReceipt = accountReceipt;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
+    }
+
+    public Account getAccountSpending() {
+        return accountSpending;
+    }
+
+    public void setAccountSpending(Account accountSpending) {
+        this.accountSpending = accountSpending;
+    }
+
+    public Account getAccountReceipt() {
+        return accountReceipt;
+    }
+
+    public void setAccountReceipt(Account accountReceipt) {
+        this.accountReceipt = accountReceipt;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
